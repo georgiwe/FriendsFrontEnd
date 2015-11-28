139eb3171e02025521a4108ed31767145e5bf59d
Georgi <georgiwe@gmail.com>
2015-11-29 00:07:12 +0200

    Fix responsive images on activities page. Add usersData service.

---
+++ app/users/usersData.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('Friends').service('usersData', ['$q', 'backendServices', function ($q, backendServices) {
+
+	function getAll () {
+		
+	}
+
+	return {
+		getAll: getAll
+	};
+
+}]);
+	
